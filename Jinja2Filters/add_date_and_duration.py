29d23c31384713fa5bcfbec2a2ced6ca48f1f07b
Chris Blanton <Chris.Blanton@an010.princeton.rdhpcs.noaa.gov>
2021-11-16 17:05:26 -0500

    New jinja function to calculate the first cycle point for timeseries generation

--- Jinja2Filters/add_date_and_duration.py
+++ Jinja2Filters/add_date_and_duration.py
@@ -1,7 +1,7 @@
 import metomi.isodatetime.parsers
 
 def add_date_and_duration(date, duration):
-    """Return the sum of a data and duration
+    """Return the sum of a date and duration
 
     Arguments:
         date (str)
@@ -12,6 +12,9 @@ def add_date_and_duration(date, duration):
     >>> add_date_and_duration('20000101T0000Z', 'P2Y')
     2002-01-01T00:00:00Z
 """
-    d = metomi.isodatetime.parsers.TimePointParser().parse(date)
+    # set the time zone to be UTC or else the cycle points will not align
+    d = metomi.isodatetime.parsers.TimePointParser(assumed_time_zone=(0,0)).parse(date)
     dur = metomi.isodatetime.parsers.DurationParser().parse(duration)
     return(d + dur)
+
+#print(add_date_and_duration('2000', 'P1Y'))
