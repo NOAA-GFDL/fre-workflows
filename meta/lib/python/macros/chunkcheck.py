d2ed60575e2caa3e4fc5127323ea150e7e085fd6
Aparna Radhakrishnan <aparna.radhakrishnan@noaa.gov>
2022-09-21 21:06:50 -0400

    validation in progress

--- meta/lib/python/macros/chunkcheck.py
+++ meta/lib/python/macros/chunkcheck.py
@@ -18,6 +18,8 @@ class ChunkChecker(metomi.rose.macro.MacroBase):
        		PP_CHUNK_A must be a multiple of HISTORY_SEGMENT
        		PP_CHUNK_B is optional. PP_CHUNK_A is set to PP_CHUNK_B if the latter is absent 
     """
+    def is_formatted_well(self, pp_chunk_a):
+               
 
     def validate(self, config, meta_config=None):
         """Return a list of errors, if any."""
@@ -30,5 +32,20 @@ class ChunkChecker(metomi.rose.macro.MacroBase):
         if not pp_chunk_a:  
                    self.add_report('template variables', 
                                     pp_chunk_a, "must exist and set to ISO8601 duration of the desired post-processed output. e.g P1Y for one year chunk")
+        else:
+                           if(self.is_formatted_well(pp_chunk_a) == False):
+                               self.add_report('Please check the value of PP_CHUNK_A and its formatting')
+                           if(self.is_multiple_of(pp_chunk_a,history_seg) == False):
+                               self.add_report(pp_chunk_a, "needs to be a multiple of ", history_seg)
+
+			   if not pp_chunk_b:		
+                               pp_chunk_b = pp_chunk_a
+                           else: 
+                               if(self.is_formatted_well(pp_chunk_b) == False):
+                                    self.add_report('Please check the value of PP_CHUNK_B and its formatting') 
+                       
+                               if(self.is_multiple_of(pp_chunk_b,pp_chunk_a) == False):
+                                    self.add_report(pp_chunk_b, "needs to be a multiple of ", pp_chunk_a)
+ 
 
         return self.reports
