d954bb9b83b36795e306f295bdf05710947d9ba2
Aparna Radhakrishnan <aparna.radhakrishnan@noaa.gov>
2022-09-28 21:23:00 -0400

    adjusted tests to include an exc testcase

--- tests/test_chunkcheck.py
+++ tests/test_chunkcheck.py
@@ -9,6 +9,12 @@ def tests_is_multiple_of():
    output = cc.is_multiple_of('P1W','P1D')
    assert output == True
 
+def teste_is_multiple_of():
+   '''Verify (exception case) True return state of `is_multiple_of`'''
+   cc = ChunkChecker()
+   output = cc.is_multiple_of('P1Y','P1M')
+   assert output == True 
+
 def testf_is_multiple_of():
    '''Verify False return state of `is_multiple_of`'''
    cc = ChunkChecker()
