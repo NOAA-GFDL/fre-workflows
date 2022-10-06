a08119d8565c413415b66293d9e1e6071f86afe5
Aparna Radhakrishnan <aparna.radhakrishnan@noaa.gov>
2022-10-06 12:00:14 -0400

    adjusted self.report to be consistent with docs

--- meta/lib/python/macros/chunkcheck.py
+++ meta/lib/python/macros/chunkcheck.py
@@ -53,16 +53,16 @@ class ChunkChecker(metomi.rose.macro.MacroBase):
         history_seg = history_seg.strip('\"') 
         #Raise error if PP_CHUNK_A value is not set
         if not pp_chunk_a: 
-            self.add_report('template variables',pp_chunk_a, "PP_CHUNK_A-- must exist and set to ISO8601 duration of the desired post-processed output. e.g P1Y for one year chunk")
+            self.add_report('template variables',"PP_CHUNK_A", " MUST exist and set to ISO8601 duration of the desired post-processed output. e.g P1Y for one year chunk")
         else:
             #Make sure the PP chunk is a multiple of the history segment value PP chunk is a multiple of the history segment value 
             if(self.is_multiple_of(pp_chunk_a,history_seg) == False):
-                self.add_report("PP_CHUNK_A (",pp_chunk_a, ") duration in days needs to be a multiple of HISTORY_SEGMENT(", history_seg,")")
+                self.add_report('template variables',"PP_CHUNK_A", f"{pp_chunk_a} duration in days needs to be a multiple of HISTORY_SEGMENT({history_seg})")
             #If P_CHUNK_B value is not set, assign PP_CHUNK_A to it 
             if not pp_chunk_b or pp_chunk_b == "":
                 print("Note: No value found for PP_CHUNK_B. Workflow will assign PP_CHUNK_A to PP_CHUNK_B")	
                 pp_chunk_b = pp_chunk_a
             else: 
                 if(self.is_multiple_of(pp_chunk_b,pp_chunk_a) == False):
-                    self.add_report("PP_CHUNK_B(",pp_chunk_b, ") duration in days needs to be a multiple of PP_CHUNK_A(", pp_chunk_a,")")
+                    self.add_report("template variables","PP_CHUNK_B", f"{pp_chunk_b} duration in days needs to be a multiple of PP_CHUNK_A ({pp_chunk_a})")
         return self.reports
