798639577a5d101a33cb7f62790eb0b17180483a
Chris Blanton <Chris.Blanton@an011.princeton.rdhpcs.noaa.gov>
2023-01-09 16:13:05 -0500

    #52 remove debugging prints

--- Jinja2Filters/divide_durations.py
+++ Jinja2Filters/divide_durations.py
@@ -7,12 +7,9 @@ def divide_durations(big_dur_str, small_dur_str):
     >>> divide_durations('P2Y', 'P1Y')
     2
 """
-    print("DEBUG HELP")
     small_dur = metomi.isodatetime.parsers.DurationParser().parse(small_dur_str)
-    print(type(small_dur_str))
     big_dur = metomi.isodatetime.parsers.DurationParser().parse(big_dur_str)
     small_dur = metomi.isodatetime.parsers.DurationParser().parse(small_dur_str)
-    print("DEBUG:", big_dur, small_dur)
     return(big_dur.get_seconds() / small_dur.get_seconds())
 
 #print(divide_durations('P2Y', 'P1Y'))
