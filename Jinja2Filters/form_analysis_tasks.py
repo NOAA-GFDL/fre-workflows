13488c8a355ebcca0fc8f8559d78e62f407c1ee6
Chris Blanton <Chris.Blanton@an010.princeton.rdhpcs.noaa.gov>
2023-02-27 14:05:11 -0500

    #52 checkpoint

--- Jinja2Filters/form_analysis_tasks.py
+++ Jinja2Filters/form_analysis_tasks.py
@@ -13,20 +13,35 @@ def strtobool(val):
         return False 
     raise Exception("invalid boolean value: {!r}".format(val))
 
-def form_analysis_tasks(pp_components_str, pp_dir, start_year, default_chunk1, default_chunk2=None):
+def get_analysis_info(pp_components_str, pp_dir, start_str, stop_str, default_chunk1_str, default_chunk2_str=None):
     """Form the analysis tasks from app/analysis/rose-app.conf
 
     Arguments:
         pp_components_str (str): all, or a space-separated list
                             analysis scripts depending on others will be skipped
         pp_dir (str): absolute filepath root (up to component, not including)
-        start_year (str): will use at yr1 if cumulative mode on
+        start_str (str): will use at yr1 if cumulative mode on
+        stop_str (str): last cycle point to process
         default_chunk[12] (str): default chunks to use if analysis script does not care
 """
+    # convert strings to date objects
+    start = metomi.isodatetime.parsers.TimePointParser().parse(start_str)
+    stop = metomi.isodatetime.parsers.TimePointParser().parse(stop_str)
+    sys.stderr.write(f"ANALYSIS: from {start} to {stop}\n")
+    default_chunk1 = metomi.isodatetime.parsers.DurationParser().parse(default_chunk1_str)
+    sys.stderr.write(f"ANALYSIS: default chunk1 is {default_chunk1}\n")
+    if default_chunk2_str:
+        default_chunk2 = metomi.isodatetime.parsers.DurationParser().parse(default_chunk2_str)
+        sys.stderr.write(f"ANALYSIS: default chunk2 is {default_chunk2}\n")
+
     pp_components = pp_components_str.split()
     path_to_conf = os.path.dirname(os.path.abspath(__file__)) + '/../app/analysis/rose-app.conf'
     node = metomi.rose.config.load(path_to_conf)
+
+    # build up the results multiline string that will contain the analysis task
     results = ""
+
+    # loop over the analysis scripts listed in the config file
     for keys, sub_node in node.walk():
         # only target the keys
         if len(keys) != 1:
@@ -39,7 +54,7 @@ def form_analysis_tasks(pp_components_str, pp_dir, start_year, default_chunk1, d
 
         # consider adding analysis script to workflow
         # if not adding, write a note why
-        sys.stderr.write(f"NOTE: Considering analysis script '{item}'\n")
+        sys.stderr.write(f"ANALYSIS: Considering '{item}'\n")
 
         # get the required pp components for the analysis script
         item_comps = node.get_value(keys=[item, 'components']).split()
@@ -49,7 +64,7 @@ def form_analysis_tasks(pp_components_str, pp_dir, start_year, default_chunk1, d
             skip_item = 0
             for comp in item_comps:
                 if comp not in pp_components:
-                    sys.stderr.write(f"NOTE: Skipping package '{item}' as it requests a component not available: '{comp}'\n")
+                    sys.stderr.write(f"ANALYSIS: Skipping '{item}' as it requests a component not available: '{comp}'\n")
                     skip_item = 1
                     continue
             if skip_item:
@@ -67,24 +82,9 @@ def form_analysis_tasks(pp_components_str, pp_dir, start_year, default_chunk1, d
         item_chunks_str = node.get_value(keys=[item, 'chunk'])
         if item_chunks_str:
             item_chunks = item_chunks_str.split()
-        elif default_chunk2:
+        elif default_chunk2_str:
             item_chunks = [default_chunk1, default_chunk2]
         else:
-            item_chunks = [default_chunk1]
-
-        # get the optional cumulative option
-        # cumulative option is the entire pp range, not just this chunk
-        # default is off
-        item_cumulative_str = node.get_value(keys=[item, 'cumulative'])
-        if item_cumulative_str:
-            item_cumulative = strtobool(item_cumulative_str)
-        else:
-            item_cumulative = False
-        if item_cumulative:
-            start_time = metomi.isodatetime.parsers.TimePointParser().parse(start_year + '-01-01')
-            sys.stderr.write(f"NOTE: Using cumulative range, starting from {start_time}\n")
-
-        # get the optional start/end options
         item_start_str = node.get_value(keys=[item, 'start'])
         item_end_str = node.get_value(keys=[item,'end'])
         #print("DEBUG:", item_start_str, item_end_str)
@@ -156,6 +156,28 @@ def form_analysis_tasks(pp_components_str, pp_dir, start_year, default_chunk1, d
         inherit = ANALYSIS-CUMULATIVE-{chunk}-{date2}, analysis-{item}
         {tail}
                 """.format(item=item, header=header, tail=tail, chunk=chunk, date2=metomi.isodatetime.dumpers.TimePointDumper().strftime(start_time, '%Y%m%d'))
+
+            # every chunk
+            else:
+                added_a_chunk = 1
+                sys.stderr.write(f"NOTE: Using regular chunks for chunk {chunk}\n")
+                results += f"""
+    {header}
+        inherit = ANALYSIS-{chunk}, analysis-{item}
+        {tail}
+                sys.stderr.write(f"NOTE: Using cumulative for chunk {chunk}\n")
+
+                # loop over the dates
+                date = start
+                while date <= stop:
+                    date_str = metomi.isodatetime.dumpers.TimePointDumper().strftime(date, '%Y%m%d')
+                    header = f"[[analysis-{item}-{chunk}-{date_str}]]"
+                    date += chunk
+                    results += f"""
+    {header}
+        inherit = ANALYSIS-CUMULATIVE-{chunk}-{date_str}, analysis-{item}
+        {tail}
+                """
 
             # every chunk
             else:
