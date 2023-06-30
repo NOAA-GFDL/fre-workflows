0078875376b9b90619854f5faf120adefa117fe0
Chris Blanton <Chris.Blanton@an008.princeton.rdhpcs.noaa.gov>
2023-06-30 12:31:47 -0400

    #2 Add timeaveraging to form_remap_dep jinja/python function. If timeaveraging is used, then the remap tasks should depend on the timeaveraging tasks succeeding as well as the ts tasks.

--- Jinja2Filters/form_remap_dep.py
+++ Jinja2Filters/form_remap_dep.py
@@ -12,7 +12,7 @@ See form_remap_dep invocations from flow.cylc  '''
 # Created by A.Radhakrishnan on 06/27/2022
 # Credit MSD workflow team
  
-def form_remap_dep(grid_type, temporal_type, chunk, pp_components_str):
+def form_remap_dep(grid_type, temporal_type, chunk, pp_components_str, do_timeavgs):
 
     """ Form the task parameter list based on the grid type, the temporal type, and the desired pp component(s)
 
@@ -101,9 +101,15 @@ def form_remap_dep(grid_type, temporal_type, chunk, pp_components_str):
               makets_stmt = ""
               for src in value:
                   if(makets_stmt != ''): 
-                    makets_stmt =  "{} & {}".format(makets_stmt,"make-timeseries-{}-{}_{}".format(grid,chunk,src))
+                      if do_timeavgs:
+                          makets_stmt =  "{} & {} & {}".format(makets_stmt,"make-timeseries-{}-{}_{}".format(grid,chunk,src), "make-timeavgs-{}-{}_{}".format(grid,chunk,src))
+                      else:
+                          makets_stmt =  "{} & {}".format(makets_stmt,"make-timeseries-{}-{}_{}".format(grid,chunk,src))
                   else:
-                    makets_stmt =  "make-timeseries-{}-{}_{}".format(grid,chunk,src)
+                      if do_timeavgs:
+                          makets_stmt =  "make-timeseries-{}-{}_{} & make-timeavgs-{}-{}_{}".format(grid,chunk,src,grid,chunk,src)
+                      else:
+                          makets_stmt =  "make-timeseries-{}-{}_{}".format(grid,chunk,src)
  
               remap_stmt = "remap-pp-components-{}_{}".format(chunk,key)
               remap_dep_stmt = "{} => {}".format(makets_stmt,remap_stmt)
