1f12d4c0ceb1fbc71a601170d30e17fc12622857
Ian Laflotte <Ian.Laflotte@noaa.gov>
2023-10-20 17:17:03 -0400

    alright! think i got the tooler to run where I wanted it to!
    next problem: the new script doesn't have execute permissions...
    that shouldn't be too bad.

--- iterate_stop_install_play.sh
+++ iterate_stop_install_play.sh
@@ -1,13 +1,14 @@
 #!/bin/bash 
-set -eox
+#set -eox
 
 #make sure RUN is defined first. source me. 
 
-cylc stop am5_c96L33_amip/run${RUN}
+cylc kill am5_c96L33_amip/run${RUN}//*
 export RUN=$(($RUN+1)) 
 bin/install-exp am5_c96L33_amip && cylc play -v -v am5_c96L33_amip/run${RUN} 
 sleep 5s 
-echo "" 
+echo "5 more s..." 
+sleep 5s
 cat ~/cylc-run/am5_c96L33_amip/run${RUN}/log/job/19800101T0000Z/pp-starter/01/job-activity.log
 
-set +eox
+#set +eox
--- lib/python/ppan_handler.py
+++ lib/python/ppan_handler.py
@@ -3,46 +3,51 @@
 #import sys
 #for path in sys.path: print(f'sys.path entry: {path}')
 
+import pathlib
 from pathlib import Path
-import shlex
 from subprocess import DEVNULL
+
+from typing import (
+    Iterable,
+    List,
+    Tuple,
+    TYPE_CHECKING,
+)
+import shlex
 
 from cylc.flow.job_runner_handlers.slurm import SLURMHandler
 from cylc.flow.cylc_subproc import procopen
 
 
 class PPANHandler(SLURMHandler):
-#class PPANHandler():
-    #SUBMIT_CMD_TMPL = "pwd; ls; echo '%(job)s'; sbatch '%(job)s'"
-    #SUBMIT_CMD_TMPL = "pwd; ls; echo '%(job)s'; sbatch '%(job)s';"
-    #SUBMIT_CMD_TMPL = "pwd; ls; echo '%(job)s'";
 
-    ## this won't work. b.c. shlex?
-    #SUBMIT_CMD_TMPL = "pwd; ls"; 
-    ## so instead we try... https://docs.python.org/3/library/shlex.html
-    #SUBMIT_CMD_TMPL = "pwd {}".format("; ls")
+    # set this to None- we want to take the approach used by job_runner_mgr
+    # when this is not None, and then stick that flow-control into submit()
+    SUBMIT_CMD_TMPL = None
 
-    ### this works!!
-    #SUBMIT_CMD_TMPL = "echo 'hello'"
-
-    ### this works!!
-    #SUBMIT_CMD_TMPL = "echo '%(job)s'"
-
-    ### this works!!
-    #SUBMIT_CMD_TMPL = "cat '%(job)s'"
-
-    ## this works!!
-    #SUBMIT_CMD_TMPL = "cat '%(job)s' && echo '%(job)s'"
-
-    ## unsure if this works, but not sure this is the way we should yet. tbh
-    ## b.c. the SLURMHandler class has no submit command to take advantage of via super()
-    #SUBMIT_CMD_TMPL = None
-
+    # internal canary/coal mine test
     @classmethod
-    def submit(
-            cls,
-            job_file_path,
-            submit_opts   ): # -> Tuple[int, str, str]:
+    def test_import(cls) -> int:
+        return 0
+
+    # internal canary/coal mine test for ops tooling
+    @classmethod
+    def test_tool_ops_import(cls) -> int:
+        import tool_ops_w_papiex
+        return tool_ops_w_papiex.test_import()
+
+    # to try to gurantee the text output isn't messed with... for now
+    @classmethod
+    def filter_submit_output(cls,
+                             out: str, err: str) -> Tuple[str, str]:
+        return (out, err)
+
+    # the thing I wish would actually work.
+    @classmethod
+    def submit(cls,
+               job_file_path: str,
+               submit_opts: dict,
+               dry_run=False   )  -> Tuple[int, str, str]:
         """Submit a job.
     
         Submit a job and return an instance of the Popen object for the
@@ -64,6 +69,7 @@ class PPANHandler(SLURMHandler):
     
         """
 
+        
         # the slurm handler has no real submit command- just a SUBMIT_CMD_TMPL
         # when job_runner_mgr.py will check for the submit, and default to using
         # SUBMIT_CMD_TMPL to form a shell command, then issued
@@ -72,83 +78,95 @@ class PPANHandler(SLURMHandler):
         # just wrapped into the submit function. 
         # to start, we copy/pasted lines 717 - 738 in cylc/flow/job_runner_mgr.py
         # below is the version i've landed on currently.
-        ret_code = None
-        out = None
-        err = None
+        ret_code = 0
+        out = ''
+        err = ''
 
-        # canary-in-coalmine output i would like to see everytime.
-        out = f'(from ppan_handler.py) hello world!\n'
-        out += f'(from ppan_handler.py) arg submit_opts = \n\n{submit_opts}\n\n' #warning, very long.
-        out += f'(from ppan_handler.py) arg job_file_path = {job_file_path}\n'
+        # canary-in-coalmine output i would like to see everytime.        
+        out+='(ppan_handler.py) hello world!\n'
 
-        err = f'(from ppan_handler.py) hello ERROR world!!!\n'
+        import tool_ops_w_papiex
+        try:
+            tool_ops_w_papiex.tool_ops_w_papiex(
+                fin_name=job_file_path,
+                fms_modulefiles=None)
+            out+='(ppan_handler.py) looks like papiex ops tooler finished OK.'
+        except:
+            err+='(ppan_handler.py) papiex ops tooler did not work.'
+
+        assert all( [ Path(job_file_path).exists(),
+                      Path(job_file_path+'.tags').exists() ] )
+        job_file_path=job_file_path+'.tags'
+        
+        if dry_run: out+='(ppan_handler.py) ======= dry_run = True ====== \n'
+        out+=f'(ppan_handler.py) arg submit_opts = \n\n{submit_opts}\n\n' #warning, very long.
+        out+=f'(ppan_handler.py) arg job_file_path = {job_file_path}\n'
+
+        err+='(ppan_handler.py) hello ERROR world!!!\n'
 
         env = submit_opts.get('env')
-        if env is None:
-            err += "submit_opts.get('env') returned None in lib/python/ppan_handler.py\n"
-            out += err
-            return 1, out, err        
-
-        submit_cmd_tmpl = "echo '%(job)s'"
-        command = shlex.split(
-            submit_cmd_tmpl % {"job": job_file_path})
-        out += 'commands are:\n'+' '.join(map(str,command))+'\n'
-        proc_stdin_arg = None
-        proc_stdin_value = DEVNULL
+        out+=f'(ppan_handler.py) submit_opts.get(\'env\') = \n {env} \n'
         
+        if env is None:
+            err+= "(ppan_handler.py) submit_opts.get('env') returned None in lib/python/ppan_handler.py\n"
+            return (1, out, err)
+
+        if dry_run:
+            cmd_tmpl = "sleep 5s"
+        else:
+            cmd_tmpl = "sbatch '%(job)s'"
+            
         try:
-            ret_code = 0
-            #pass
+            command = shlex.split(
+                cmd_tmpl % {"job": job_file_path})
+        except:
+            err+=f"(ppan_handler.py) shlex.split(\n    {cmd_tmpl} % {'job':{job_file_path}}\n\n did not work.\n"
+            command=shlex.split(
+                cmd_tmpl)
+
+
+        out+='(ppan_handler.py) command is: '
+        for part in command:
+            out+=f' {part}'
+        out+='\n'                
+
+        
+        
+        proc_stdin_value = DEVNULL
+        try:
+            cwd = pathlib.Path('~').expanduser()
+            out+=f'(ppan_handler.py) cwd={str(cwd)}\n'
+            
             proc = procopen(
                 command,
-                stdin      = proc_stdin_arg,
-                stdindevnull=None,
-                splitcmd = False,
+                stdin=None,
                 stdoutpipe = True,
                 stderrpipe = True,
                 env = env,
-                # paths in directives should be interpreted relative to 
-                # $HOME                                                 
+                # paths in directives should be interpreted relative to  $HOME                                                 
                 # https://github.com/cylc/cylc-flow/issues/4247         
-                cwd = Path('~').expanduser()
+                cwd = cwd
             )
         except OSError as exc:
             # subprocess.Popen has a bad habit of not setting the       
             # filename of the executable when it raises an OSError.
-            out +='\n !!!!!!!!!!!!!!!!!!!!OSError as exc!!!!!!!!!!!!!!!!!!!!!!\n\n'
+            out+='\n (ppan_handler.py) !!!!!!!!!!!!!!!!!!!!OSError as exc!!!!!!!!!!!!!!!!!!!!!!\n\n'
+            err+='\n (ppan_handler.py) !!!!!!!!!!!!!!!!!!!!OSError as exc!!!!!!!!!!!!!!!!!!!!!!\n\n'
             if not exc.filename:
-                exc.filename = command[0]
-            err += '\n'+str(exc)
-            #return 1, out, err
-            ret_code = 1
+                exc.filename = command[0]                        
+            return 1, out, err
 
-        out1, err1 = (f.decode() for f in proc.communicate(proc_stdin_value))
-        #ret_code = proc.wait()        
-        return ret_code, out, err
+        proc_out, proc_err = (f.decode() for f in proc.communicate(proc_stdin_value))
+        out+=f'(ppan_handler.py) procopen output: {proc_out}\n'
+        err+=f'(ppan_handler.py) procopen err output: {proc_err}\n'
+        try:
+            ret_code = proc.wait()
+        except:
+            err+='(ppan_handler.py) proc.wait() did not work. moving on.'
+            ret_code=1
+
+        return (ret_code, out, err)
 
 # based on
 #https://cylc.github.io/cylc-doc/stable/html/plugins/job-runners/index.html
 JOB_RUNNER_HANDLER = PPANHandler()
-
-
-
-##in terms of she-bangs at the start of this, we have two options:
-##### result:
-###!/app/conda/miniconda/envs/cylc/bin/python
-
-##### result:
-###!/usr/bin/env python3
-
-
-### result:
-#from cylc.flow.job_runner_handlers.slurm import SlurmHandler
-
-## result:
-#import cylc
-#from cylc import flow.job_runner_handlers.slurm.SlurmHandler as SlurmHandler
-
-## result:
-#import cylc
-#from cylc import flow.job_runner_handlers.slurm.SlurmHandler
-
-
