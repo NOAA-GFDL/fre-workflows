858434c896dc12c1d47441cc12972c1bf54ed267
Cole Harvey <cah062003@gmail.com>
2024-03-20 15:42:30 -0400

    outputs hash to stdout, not sure how I will improve this currently

--- data_lineage/bloomfilter/Bloomfilter.py
+++ data_lineage/bloomfilter/Bloomfilter.py
@@ -1,30 +1,34 @@
 import mmh3
-# from bitarray import bitarray
-import array
-import bitarray
-
-size = 2 ** 13
-hash_count = 100
+from Bitarray import BitArray
 
 
-class BloomFilter(object):
-    def __init__(self):
+class BloomFilter:
+    def __init__(self, size, hash_count):
         self.size = size
         self.hash_count = hash_count
+        self.bit_array = BitArray(self.size)
 
     def hash(self, item):
         """
         1. Creates a bit array
-        2. Hashes the item 'hash_count' times
+        2. Hashes the item 'hash_count' times using MurmurHash3
         3. Modulus the hash by the size of the bitarray
         4. Update cell[modulus-value] = !cell
         """
-        bit_array = my_bitarray.BitArray(self.size)
-
         for i in range(self.hash_count):
             digest = mmh3.hash(item, i) % self.size
 
             # bit_array[digest] = not bit_array[digest]
-            bit_array.set_bit(index=digest, value=1)
+            self.bit_array.set_bit(index=digest, value=1)
 
-        return bit_array
+        return self.bit_array
+
+    def contains(self, item):
+        """
+        Mainly used to verify a bitarray matches with a provided input
+        """
+        for i in range(self.hash_count):
+            digest = mmh3.hash(item, i) % self.size
+            if not self.bit_array.get_bit(digest):
+                return False
+        return True
--- data_lineage/bloomfilter/HashGen.py
+++ data_lineage/bloomfilter/HashGen.py
@@ -1,28 +1,68 @@
+import subprocess
 import time
-import mmh3
-from sys import argv
-from bloomfilter import BloomFilter
+import sys
+import netCDF4 as nc
+from Bloomfilter import BloomFilter
+
 
 # ncks -M /archive/Cole.Harvey/canopy/am5/c96L33_amip/pp/atmos/av/monthly_2yr/atmos.1980-1981.11.nc
 
+# /home/Cole.Harvey/.conda/envs/bloom-filter-env/bin/python
 
-def main():
 
-    bf = BloomFilter()
-    print(f'Size of bit array : {bf.size}')
-    print(f'Number of hash functions : {bf.hash_count}')
-    print(f'Hash Power : {bf.size * bf.hash_count}')
+def scrape_metadata(file_path):
+    rootgrp = nc.Dataset(file_path, "r")
+    meta_data = ''
 
-    path = "test"
-    file = path * 1_000_000
+    for dim_name, dim in rootgrp.dimensions.items():
+        dim_header = str(f'{dim_name} = {len(dim)}\n')
+        meta_data += dim_header
+    for var_name, var in rootgrp.variables.items():
+        var_header = str(f'{var.dtype} {var_name}({var.dimensions}) ;\n')
+        meta_data += var_header
+
+    # history = rootgrp.history
+    # meta_data += history
+
+    rootgrp.close()
+    return meta_data
+
+
+def generate_hash(meta_data):
+    # scrape_metadata(file_path)
+
+    # Keep size a power of 2
+    size = 2 ** 13
+    hash_count = 100
+
+    bf = BloomFilter(size, hash_count)
+    # print(f'Size of bit array : {bf.size}')
+    # print(f'Number of hash functions : {bf.hash_count}')
+    # print(f'Hash Power : {bf.size * bf.hash_count}')
 
     start_time = time.time()
-    bit_array = bf.hash(file)
-    hash_value = bit_array.condense()
+    bit_array = bf.hash(meta_data)
+    file_hash = bit_array.condense()
 
-    print(f'--- %s seconds to create bitarray for {path}---' % (time.time() - start_time))
-    print(f'generated hash for {path} : {hash_value}')
+    # print(f'--- %s seconds to create bitarray ---' % (time.time() - start_time))
+
+    return file_hash
+
+
+def main(file_path):
+    # file_path = '/archive/Cole.Harvey/canopy/am5/c96L33_amip/pp/atmos/av/monthly_2yr/atmos.1980-1981.11.nc'
+
+    meta_data = scrape_metadata(file_path)
+    file_hash = generate_hash(meta_data)
+    # print(f'generated hash for {file_path} : {file_hash}')
+
+    print(file_hash)
+
+    return file_hash
 
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) < 2:
+        print("Usage: python HashGen.py <filepath>")
+        sys.exit(1)
+    main(sys.argv[1])
