a8f79c3f9a710b9b396f50dbcc41402680719f88
Cole Harvey <cah062003@gmail.com>
2024-05-07 10:11:00 -0400

    better documentation and readability

--- data_lineage/bloomfilter/StringCompression.py
+++ data_lineage/bloomfilter/StringCompression.py
@@ -3,24 +3,34 @@ import base64
 import sys
 import logging
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+ORIGINAL_SIZE_FORMAT = 'size of input string: {}'
+COMPRESSED_SIZE_FORMAT = 'size of compressed bytes: {}'
+COMPRESSED_B64_SIZE_FORMAT  = 'size of compressed base64 bytes: {}'
+DECOMPRESSED_SIZE_FORMAT = 'size of decompressed bytes: {}'
 
 def compress_string(input_string):
+    """
+    Called directly in the bash script. Compresses input_string into a base64
+    representation of the compressed bytes from the python library gzip.
 
+    The size of compression varies, but any string with a size > 72 bytes
+    typically saves space with this compression method.
+    """
     input_bytes = input_string.encode('utf-8')
     compressed_bytes = gzip.compress(input_bytes)
     b64_bytes = base64.b64encode(compressed_bytes)
 
-    size_original = sys.getsizeof(input_string)
-    size_compressed = sys.getsizeof(compressed_bytes)
-    size_b64 = sys.getsizeof(b64_bytes)
-
-    logging.info(f'size of input string                      : {size_original}')
-    logging.info(f'size of input string in utf-8 bytes       : {size_compressed}')
-    logging.info(f'size of input string in b64 bytes         : {size_b64}')
+    logging.info(ORIGINAL_SIZE_FORMAT.format(sys.getsizeof(input_string)))
+    logging.info(COMPRESSED_SIZE_FORMAT.format(sys.getsizeof(compressed_bytes)))
+    logging.info(COMPRESSED_B64_SIZE_FORMAT.format(sys.getsizeof(b64_bytes)))
 
     b64_string = str(b64_bytes)[2:-1]
 
     # https://en.wikipedia.org/wiki/Base64#:~:text=Output%20padding%5B,the%20output%20padding%3A
+    # Bash scripts handle `=` weirdly. To avoid this, replace the buffers with the
+    # temporary string representation  `_pad`
     b64_string = b64_string.replace('=', '_pad')
 
     print(b64_string)
@@ -28,28 +38,29 @@ def compress_string(input_string):
     return b64_string
 
 
-def decompress_bytes(_bytes):
-    b64_decoded_bytes = base64.b64decode(_bytes)
+def decompress_bytes(b64_string):
+    """
+    Reverts the compression for the file annotation.
+    """
+
+    # Base64 encodes '=' as padding at the end since it uses 24-bit sequences
+    # https://stackoverflow.com/questions/38763771/how-do-i-remove-double-back-slash-from-a-bytes-object
+    b64_string = b64_string.replace('_pad', '=')
+    compressed_data = b64_string.encode().decode('unicode_escape').encode("raw_unicode_escape")
+
+    b64_decoded_bytes = base64.b64decode(compressed_data)
     decompressed_bytes = gzip.decompress(b64_decoded_bytes)
     decompressed_string = str(decompressed_bytes)[2:-1]
-    len_str = sys.getsizeof(decompressed_string)
 
-    logging.info(f'size of decompressed bytes          : {len_str}')
-
-    # print(str(decompressed_string)[2:-1])
+    logging.info(DECOMPRESSED_SIZE_FORMAT.format(sys.getsizeof(decompressed_string)))
 
     return decompressed_string
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
     if len(sys.argv) != 2:
         logging.info(sys.argv)
-        # compress_string('19810101.land_daily_cmip.tile1.nc  1eed5b1bbde501,19810101.land_daily_cmip.tile2.nc
-        # 1dc1f4b980e4f,19810101.land_daily_cmip.tile3.nc  ec8a56a6fe669,19810101.land_daily_cmip.tile4.nc
-        # 1b89f8e65223f2,19810101.land_daily_cmip.tile5.nc  bb3b2c0fe1f24,19810101.land_daily_cmip.tile6.nc
-        # 111ef0b6a1a31c')
         logging.error("Usage: python StringCompression.py <str>")
         sys.exit(1)
 
