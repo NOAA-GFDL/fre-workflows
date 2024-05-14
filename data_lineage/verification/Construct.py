534d210fdadf79c4e0b38e402670ccd643fae44d
Cole Harvey <cah062003@gmail.com>
2024-05-14 16:16:00 -0400

    added documentation

--- data_lineage/verification/Construct.py
+++ data_lineage/verification/Construct.py
@@ -3,55 +3,97 @@ from data_lineage.dag.Node import Node
 from data_lineage.dag.Edge import Edge
 
 
-def process_graph(data):
+def process_graph(config_data):
+    """
+    Constructs the configuration DAG from data that was scraped from a
+    cylc-run's configuration file, which is found at
+    cylc-run/exp/runN/log/config/01-start-01.cylc.
 
+    1. Iterate through the different chunks. Eg: P1Y, P2Y ...
+    2. Iterate through the step in the chunk, and deconstruct each sequence
+       of jobs in the step.
+    3. Within this deconstructed sequence, add each exact job to the dag
+       as nodes, and use the order of the sequence to add the edges
+       between the nodes.
+
+    Args:
+        config_data: Dict
+            The parsed data from the 01-start-01.cylc configuration file.
+
+    Returns:
+        dag: Dag
+            A Dag object of the configuration DAG. Uses the Dag class created
+            in data_lineage/dag/Dag.py. Does not contain any i/o files or hashes.
+    """
     dag = DAG()
 
-    for chunk in data['graph']:
-        prefixes = handle_chunk(chunk, data)
+    for chunk in config_data['graph']:
+        prefixes = handle_chunk(chunk, config_data)
 
         if not prefixes:
             continue
 
-        for step in data['graph'][chunk]:
-            previous_jobs = None
+        for step in config_data['graph'][chunk]:
+            previous_jobs = []
 
             jobs = step.split(' => ')
 
             for job in jobs:
-
                 current_jobs = job.split(' & ')
+                current_exact_jobs = []
+                temporary_prefixes = []
 
                 for current_job in current_jobs:
+                    # If a job contains the [-P1Y] tag, it is in the 'R1' chunk and is referring to the 'P1Y' version
+                    if '[-P1Y]' in current_job:
+                        duration = int(config_data['end']) - int(config_data['start'])
 
+                        for i in range(duration + 1):
+                            temporary_prefixes.append(str(int(config_data['start']) + i))
+
+                    # All versions must be completed with a successful return
                     if ':succeed-all' in job:
                         job_name = job.split(':')[0].lower()
                         exact_jobs = find_all_nodes_by_name(job_name, dag)
                     else:
                         job_name, job_param = parse_job(current_job)
-                        exact_jobs = generate_exact_jobs(job_name, job_param, data, prefixes)
+                        exact_jobs = generate_exact_jobs(job_name, job_param, config_data, temporary_prefixes if temporary_prefixes else prefixes)
 
                     for exact_job in exact_jobs:
+                        current_exact_jobs.append(exact_job)
 
                         if not dag.find_node(exact_job):
                             dag.add_node(job_name=exact_job)
 
                         if previous_jobs:
-
                             for previous_exact_job in previous_jobs:
                                 dag.add_edge(dag.find_node((previous_exact_job)), dag.find_node(exact_job))
 
-                previous_jobs = exact_jobs
+                previous_jobs = current_exact_jobs
 
     dag.dag_print()
 
     return dag
 
 
-def handle_chunk(chunk, data):
+def handle_chunk(chunk, config_data):
+    """
+    Generates the prefixes for jobs in a given chunk. Prefixes are the frequency
+    of a job in a workflow.
+
+    If a workflow's duration is 2000 to 2004, and the chunk was 'P2Y', the prefixes
+    would be [2000, 2002, 2004].
+
+    Args:
+        chunk: String
+            Frequency of job occurances.
+        config_data: Dict
+            The parsed data from the 01-start-01.cylc configuration file.
+
+    """
     prefixes = []
-    start = data['start']
-    end = data['end']
+    start = config_data['start']
+    end = config_data['end']
     duration = end - start + 1
 
     if chunk == 'R1':
@@ -77,6 +119,20 @@ def handle_chunk(chunk, data):
 
 
 def parse_job(job):
+    """
+    Deconstructs a job into two separate fields.
+
+    Args:
+        job: String
+            Singular job from config_data
+
+    Returns:
+        job_name: String
+            The generic name of the job as seen in the `app/` directory
+            or the 'site/ppan.cylc' file
+        job_param: String
+            The task parameter the job is using.
+    """
     # Ignore parameters indicated by a '<' character, remove any & signs from multi-line jobs
     # in the config, strip any leading/trailing whitespace, and then convert to lowercase
     job_name = job.split('<')[0].replace('&', '').strip().lower()
@@ -92,29 +148,53 @@ def parse_job(job):
     return job_name, job_param
 
 
-def generate_exact_jobs(job_name, job_param, data, prefixes):
+def generate_exact_jobs(job_name, job_param, config_data, prefixes):
+    """
+    Create a list of exact jobs with years and corresponding task parameters.
+    Uses the same format that was used when creating nodes for the serial DAG.
+
+    Iterate over the years the job is run or the different parameters the job
+    uses, and create a list of these.
+
+    Format should be: YYYY_jobname_taskparameter
+
+    Args:
+        job_name: String
+            The generic job name as seen in the `app/` directory or `site/ppan.cylc`
+        job_param: String
+            The specific group of task parameters the job uses
+        config_data: Dict
+            The parsed data from the 01-start-01.cylc configuration file.
+        prefixes: List
+            List of different years this job was run for
+
+    Returns:
+        exact_jobs: List
+            List of jobs with specific years and task parameters added to it
+    """
     exact_jobs = []
 
     if job_param:
         exact_jobs = [prefix + '_' + job_name + '_' + param for prefix in prefixes
-                      for param in data['task_parameters'][job_param]]
+                      for param in config_data['task_parameters'][job_param]]
     else:
         exact_jobs = [prefix + '_' + job_name for prefix in prefixes]
 
     return exact_jobs
 
 
-def generate_previous_exact_jobs(previous_jobs, data, prefixes):
-    previous_exact_jobs = []
-
-    for job in previous_jobs:
-        job_name, job_param = parse_job(job)
-        previous_exact_jobs = generate_exact_jobs(job_name, job_param, data, prefixes)
-
-    return previous_exact_jobs
-
-
 def find_all_nodes_by_name(job_name, dag):
+    """
+    Searches for all nodes in a dag that share a generic job_name.
+
+    Args:
+        job_name: String
+            The generic job name as seen in the `app/` directory or `site/ppan.cylc`
+
+    Returns:
+        dag: Dag
+            The configuration file DAG
+    """
     nodes = []
 
     for node in dag.get_nodes():
