0d3e79b922f7ce824aaeaa86d48c7e368b45d48c
Cole Harvey <cah062003@gmail.com>
2024-06-03 15:31:56 -0400

    documentation and improved methods for running the tool

--- data_lineage/Run.py
+++ data_lineage/Run.py
@@ -1,12 +1,23 @@
+import sys
 from data_lineage.dag.FetchData import main as fetch_main
 from data_lineage.dag.SerialDag import main as generate_serial_dag
 from data_lineage.verification.Validate import main as validate
 from data_lineage.dag.Visualize import draw
 
 
-def main():
-    fp = '21bc35b4-f6bf-40c7-9bbb-027dfcd436f0'
-    jobs, run_dir = fetch_main(fp)
+def main(fingerprint):
+    """
+    Controls the entire data lineage tool.
+        1. Fetches jobs from EPMT
+        2. Creates the serial dag
+        3. Verifies with configuration dag
+        4. Draws the serial dag
+
+    Args:
+        fingerprint: String
+            The CYLC_WORKFLOW_UUID generated from a cylc workflow.
+    """
+    jobs, run_dir = fetch_main(fingerprint)
 
     serial_dag = generate_serial_dag(jobs, run_dir)
 
@@ -16,4 +27,14 @@ def main():
     print("Successfully finished running, ending program.")
 
 if __name__ == "__main__":
-    main()
+    # If not passing in a fingerprint to Run.py, use the provided fingerprint instead.
+    if len(sys.argv) == 1:
+        fingerprint = '21bc35b4-f6bf-40c7-9bbb-027dfcd436f0'
+    elif len(sys.argv) == 2:
+        fingerprint = sys.argv[1]
+    else:
+        print('ERROR: Incorrect args. Please either specificy a fingerprint, or '
+              'pass in 0 args to use the fingerprint provided in Run.py.')
+        sys.exit(1)
+
+    main(fingerprint)
