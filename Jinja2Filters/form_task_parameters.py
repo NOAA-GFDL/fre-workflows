7b44f1c5cfcbddf2dd6003d7242928333e77ca37
Chris Blanton <Chris.Blanton@ldt-4359643.gfdl.noaa.gov>
2025-03-01 18:10:21 -0500

    Convert arrays in string form to actual array and logging

--- Jinja2Filters/form_task_parameters.py
+++ Jinja2Filters/form_task_parameters.py
@@ -1,6 +1,13 @@
 import re
 import os
 import metomi.rose.config
+import ast
+
+# set up logging
+import logging
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 def form_task_parameters(grid_type, temporal_type, pp_components_str):
     """Form the task parameter list based on the grid type, the temporal type,
@@ -11,8 +18,8 @@ def form_task_parameters(grid_type, temporal_type, pp_components_str):
         temporal_type (str): One of: temporal or static
         pp_component (str): all, or a space-separated list
 """
+    logger.debug(f"Desired pp components: {pp_components_str}")
     pp_components = pp_components_str.split()
-    #print("DEBUG: desired pp components:", pp_components)
     path_to_conf = os.path.dirname(os.path.abspath(__file__)) + '/../app/remap-pp-components/rose-app.conf'
     node = metomi.rose.config.load(path_to_conf)
     results = []
@@ -27,15 +34,16 @@ def form_task_parameters(grid_type, temporal_type, pp_components_str):
         if item == "env" or item == "command":
             continue
         comp = regex_pp_comp.match(item).group()
-        #print("DEBUG: Examining", item, comp)
+        logger.debug(f"Examining item '{item}' comp '{comp}'")
 
         # skip if pp component not desired
-        if "all" not in pp_components:
-            #print("DEBUG: PP COMP", pp_components, "COMP is", comp)
-            if comp not in pp_components: 
-       		#print("DEBUG2: comp not in pp_components", pp_components, "and", comp)
-                continue
-        #print("DEBUG: Examining", item, comp)
+        logger.debug(f"Is {comp} in {pp_components}?")
+        if comp in pp_components: 
+            logger.debug('Yes')
+            pass
+        else:
+            loger.debug('No')
+            continue
 
         # skip if grid type is not desired
         # some grid types (i.e. regrid-xy) have subtypes (i.e. 1deg, 2deg)
@@ -43,7 +51,7 @@ def form_task_parameters(grid_type, temporal_type, pp_components_str):
         # So we will strip off after the slash and the remainder is the grid type
         candidate_grid_type = re.sub('\/.*', '', node.get_value(keys=[item, 'grid']))
         if candidate_grid_type != grid_type:
-            #print("DEBUG: Skipping as not right grid; got", candidate_grid_type, "and wanted", grid_type)
+            logger.debug(f"Skipping as not right grid; got '{candidate_grid_type}' and wanted '{grid_type}'")
             continue
 
         # filter static and temporal
@@ -52,22 +60,23 @@ def form_task_parameters(grid_type, temporal_type, pp_components_str):
         # if freq does not include "P0Y" => temporal
         freq = node.get_value(keys=[item, 'freq'])
         if freq is not None and 'P0Y' in freq and temporal_type == 'temporal':
-            #print("DEBUG: Skipping static when temporal is requested")
+            logger.debug("Skipping static when temporal is requested")
             continue
         if temporal_type == "static":
             if freq is not None and 'P0Y' not in freq:
-                #print("DEBUG: Skipping as static is requested, no P0Y here", freq)
+                logger.debug("Skipping as static is requested, no P0Y here", freq)
                 continue
         elif (temporal_type == "temporal"):
             if freq is not None and 'P0Y' in freq:
-                #print("DEBUG: Skipping as temporal is requested, P0Y here", freq)
+                logger.debug("Skipping as temporal is requested, P0Y here", freq)
                 continue
         else:
             raise Exception("Unknown temporal type:", temporal_type)
 
-        results = results + node.get_value(keys=[item, 'sources']).split()
+        # convert array in string form to array
+        sources = ast.literal_eval(node.get_value(keys=[item, 'sources']))
+        results.extend(sources)
 
     answer = sorted(list(set(results)))
+    logger.debug("Returning string" + ', '.join(answer))
     return(', '.join(answer))
-
-#print(form_task_parameters('native', 'temporal', 'land atmos land_cubic'))
