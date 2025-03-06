cd4967ebe5f2fabfd123d9248a1ad569438bce37
Dana Singh <Dana.Singh@ldt-4742397.gfdl.noaa.gov>
2025-03-06 14:33:26 -0500

    #48 Read combined yaml directly for source information

--- Jinja2Filters/form_task_parameters.py
+++ Jinja2Filters/form_task_parameters.py
@@ -1,8 +1,8 @@
-import re
+#import re
 import os
-import metomi.rose.config
-import ast
-from pathlib import Path
+#import metomi.rose.config
+#import ast
+from pathlib import Path 
 import yaml
 
 # set up logging
@@ -19,11 +19,11 @@ def form_task_parameters(grid_type, temporal_type, pp_components_str, yamlfile):
         grid_type (str): One of: native or regrid-xy
         temporal_type (str): One of: temporal or static
         pp_component (str): all, or a space-separated list
-"""
+    """
     logger.debug(f"Desired pp components: {pp_components_str}")
     pp_components = pp_components_str.split()
-    path_to_conf = os.path.dirname(os.path.abspath(__file__)) + '/../app/remap-pp-components/rose-app.conf'
-    node = metomi.rose.config.load(path_to_conf)
+#    path_to_conf = os.path.dirname(os.path.abspath(__file__)) + '/../app/remap-pp-components/rose-app.conf'
+#    node = metomi.rose.config.load(path_to_conf)
 
     # Path to yaml configuration
     exp_dir = Path(__file__).resolve().parents[1]
@@ -32,20 +32,13 @@ def form_task_parameters(grid_type, temporal_type, pp_components_str, yamlfile):
     with open(path_to_yamlconfig,'r') as yml:
         yml_info = yaml.safe_load(yml)
 
+    #
     results = []
-    regex_pp_comp = re.compile('^\w+')
-    for keys, sub_node in node.walk():
-        # only target the keys
-        if len(keys) != 1:
-            continue
+    for comp_info in yml_info["postprocess"]["components"]:
+        comp = comp_info.get("type")
 
-        # skip env and command keys
-        item = keys[0]
-        if item == "env" or item == "command":
-            continue
-        comp = regex_pp_comp.match(item).group()
-        logger.debug(f"Examining item '{item}' comp '{comp}'")
-
+        # Check that pp_components defined matches those in the yaml file
+        # Skip component if they don't match
         # skip if pp component not desired
         logger.debug(f"Is {comp} in {pp_components}?")
         if comp in pp_components: 
@@ -54,38 +47,68 @@ def form_task_parameters(grid_type, temporal_type, pp_components_str, yamlfile):
             logger.debug('No')
             continue
 
-        # skip if grid type is not desired
-        # some grid types (i.e. regrid-xy) have subtypes (i.e. 1deg, 2deg)
-        # in remap-pp-components/rose-app.conf the grid type and subgrid is specified as "regrid-xy/1deg" (e.g.).
-        # So we will strip off after the slash and the remainder is the grid type
-        candidate_grid_type = re.sub('\/.*', '', node.get_value(keys=[item, 'grid']))
+#### CHECK THIS SECTION ####
+        # Set grid type if component has xyInterp defined or not
+        if "xyInterp" not in comp_info.keys():
+            candidate_grid_type = "native"
+        else:
+            candidate_grid_type = "regrid-xy"
+
+        # Check that candidate_grid_type matches grid type passed in function
+        # If not, skip post-processing of component
         if candidate_grid_type != grid_type:
             logger.debug(f"Skipping as not right grid; got '{candidate_grid_type}' and wanted '{grid_type}'")
             continue
 
-        # filter static and temporal
-        # if freq is not set             => temporal
-        # if freq includes "P0Y"         => static
-        # if freq does not include "P0Y" => temporal
-        freq = node.get_value(keys=[item, 'freq'])
-        if freq is not None and 'P0Y' in freq and temporal_type == 'temporal':
-            logger.debug("Skipping static when temporal is requested")
-            continue
+## original
+#        # filter static and temporal
+#        # if freq is not set             => temporal
+#        # if freq includes "P0Y"         => static
+#        # if freq does not include "P0Y" => temporal
+#        freq = node.get_value(keys=[item, 'freq'])
+#        if freq is not None and 'P0Y' in freq and temporal_type == 'temporal':
+#            logger.debug("Skipping static when temporal is requested")
+#            continue
+#        if temporal_type == "static":
+#            if freq is not None and 'P0Y' not in freq:
+#                logger.debug("Skipping as static is requested, no P0Y here", freq)
+#                continue
+#        elif (temporal_type == "temporal"):
+#            if freq is not None and 'P0Y' in freq:
+#                logger.debug("Skipping as temporal is requested, P0Y here", freq)
+#                continue
+#        else:
+#            raise Exception("Unknown temporal type:", temporal_type)
+#
+#        # convert array in string form to array
+#        sources = ast.literal_eval(node.get_value(keys=[item, 'sources']))
+#        results.extend(sources)
+
+## rewrite
+        # Filter static and temporal
         if temporal_type == "static":
-            if freq is not None and 'P0Y' not in freq:
-                logger.debug("Skipping as static is requested, no P0Y here", freq)
+            #print(comp_info["static"]["freq"])
+            if "static" not in comp_info.keys():
+                logger.debug("Skipping static as there is no static source")
                 continue
-        elif (temporal_type == "temporal"):
-            if freq is not None and 'P0Y' in freq:
-                logger.debug("Skipping as temporal is requested, P0Y here", freq)
-                continue
+
+            results = comp_info.get("static").get("sources")
+
+        elif temporal_type == "temporal":
+            results = results + comp_info.get("sources")
+
         else:
-            raise Exception("Unknown temporal type:", temporal_type)
-
-        # convert array in string form to array
-        sources = ast.literal_eval(node.get_value(keys=[item, 'sources']))
-        results.extend(sources)
-
+            raise Exception(f"Unknown temporal type: {temporal_type}")
+            
+    # results list --> set --> list: checks for repetitive sources listed
     answer = sorted(list(set(results)))
+
+    # Returns a comma separated list of sources
     logger.debug("Returning string" + ', '.join(answer))
     return(', '.join(answer))
+
+## TESTING ##
+#print(form_task_parameters('regrid-xy', 'temporal', 'ocean_cobalt_sfc ocean_cobalt_btm', 'COBALT_postprocess.yaml')))
+#print(form_task_parameters('regrid-xy', 'static', 'ocean_cobalt_sfc ocean_cobalt_btm', 'COBALT_postprocess.yaml'))
+#print(form_task_parameters('native', 'temporal', 'ocean_cobalt_sfc ocean_cobalt_btm', 'COBALT_postprocess.yaml'))
+#print(form_task_parameters('native', 'static', 'ocean_cobalt_sfc ocean_cobalt_btm', 'COBALT_postprocess.yaml'))
