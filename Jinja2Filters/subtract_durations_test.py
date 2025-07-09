6db86a76c8e92ee0f6630a171863dd5de0bcf174
Chris Blanton <Chris.Blanton@an106.princeton.rdhpcs.noaa.gov>
2025-07-08 22:06:43 -0400

    #88 use pytest.mark.parametrize for duplicated tests

--- Jinja2Filters/subtract_durations_test.py
+++ Jinja2Filters/subtract_durations_test.py
@@ -2,14 +2,13 @@ import pytest
 import metomi.isodatetime.parsers
 from . import subtract_durations
 
-def test_subtraction():
-    P1D = metomi.isodatetime.parsers.DurationParser().parse('P1D')
-    P1M = metomi.isodatetime.parsers.DurationParser().parse('P1M')
-    P2M = metomi.isodatetime.parsers.DurationParser().parse('P2M')
-    P1Y = metomi.isodatetime.parsers.DurationParser().parse('P1Y')
-
-    assert P1D == subtract_durations.subtract_durations('P10D', 'P9D')
-    assert P1M == subtract_durations.subtract_durations('P2M', 'P1M')
-    assert P1M == subtract_durations.subtract_durations('P3M', 'P2M')
-    assert P2M == subtract_durations.subtract_durations('P3M', 'P1M')
-    assert P1Y == subtract_durations.subtract_durations('P2Y', 'P1Y')
+@pytest.mark.parametrize("minuend, subtrahend, difference",
+    [('P10D', 'P9D', 'P1D'),
+     ('P2M', 'P1M', 'P1M'),
+     ('P3M', 'P2M', 'P1M'),
+     ('P3M', 'P1M', 'P2M'),
+     ('P2Y', 'P1Y', 'P1Y')])
+def test_subtraction(minuend, subtrahend, difference):
+    """Test some common date subtractions"""
+    assert metomi.isodatetime.parsers.DurationParser().parse(difference) \
+    == subtract_durations.subtract_durations(minuend, subtrahend)
