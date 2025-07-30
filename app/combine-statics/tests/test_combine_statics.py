6c5033377506f9242450bc4faf720a80dbe3a65e
Dana Singh <Dana.Singh@ldt-4742397.gfdl.noaa.gov>
2025-07-30 11:01:16 -0400

    #89 Update combine-statics test doc and address some pylint messages

--- app/combine-statics/tests/test_combine_statics.py
+++ app/combine-statics/tests/test_combine_statics.py
@@ -1,18 +1,23 @@
+"""
+Test combine-statics task script
+"""
 import os
 import subprocess
 import shutil
 from pathlib import Path
-import pytest
-import subprocess
-import netCDF4
+from netCDF4 import Dataset
 
 COMBINE_STATICS_DIR = Path(__file__).resolve().parents[1]
 TEST_DIR = Path(f"{COMBINE_STATICS_DIR}/tests")
 DATA_DIR = Path(f"{TEST_DIR}/test-data")
 
 COMP_NAME = "atmos_static_scalar"
-STATIC_DATA_CDLFILE = ["atmos_static_scalar.bk1.cdl", "atmos_static_scalar.bk2.cdl", "atmos_static_scalar.bk3.cdl"]
-STATIC_DATA_NCFILE  = ["atmos_static_scalar.bk1.nc", "atmos_static_scalar.bk2.nc", "atmos_static_scalar.bk3.nc"]
+STATIC_DATA_CDLFILE = ["atmos_static_scalar.bk1.cdl",
+                       "atmos_static_scalar.bk2.cdl",
+                       "atmos_static_scalar.bk3.cdl"]
+STATIC_DATA_NCFILE  = ["atmos_static_scalar.bk1.nc",
+                       "atmos_static_scalar.bk2.nc",
+                       "atmos_static_scalar.bk3.nc"]
 
 # Output dir from generating netcdf files
 NCGEN_OUT = f"{TEST_DIR}/ncgen_output/atmos_static_scalar"
@@ -27,10 +32,7 @@ else:
     Path(f"{NCGEN_OUT}/P0Y/P0Y").mkdir(parents=True,exist_ok=True)
     Path(COMBINE_STATICS_OUT).mkdir(parents=True,exist_ok=True)
 
-#make combine-statics output dir
-#Path(COMBINE_STATICS_OUT).mkdir(parents=True,exist_ok=True)
-
-def test_cdl_file_exists(capfd):
+def test_cdl_file_exists():
     """
     Test for the existence of static cdl test files
     """
@@ -38,16 +40,17 @@ def test_cdl_file_exists(capfd):
                 Path(f"{DATA_DIR}/{STATIC_DATA_CDLFILE[1]}").exists(),
                 Path(f"{DATA_DIR}/{STATIC_DATA_CDLFILE[2]}").exists()])
 
-def test_ncgen_static_nc_files(capfd):
+def test_ncgen_static_nc_files():
     """
-    test..................
+    Generate netcdf files from cdl files; test they were created
+    successfully and exist
     """
     # NCGEN command: ncgen -o [outputfile] [inputfile]
     for num in range(3):
         inputfile = STATIC_DATA_CDLFILE[num]
         outputfile = STATIC_DATA_NCFILE[num]
         output_path = f"{TEST_DIR}/ncgen_output/{COMP_NAME}/P0Y/P0Y/{outputfile}"
- 
+
         ex = ["ncgen", "-k", "64-bit offset",
               "-o", output_path,
               f"{DATA_DIR}/{inputfile}"]
@@ -60,44 +63,49 @@ def test_ncgen_static_nc_files(capfd):
         # 2. nc file creation
         assert all([sp.returncode == 0,
                     Path(f"{TEST_DIR}/ncgen_output/{COMP_NAME}/P0Y/P0Y/{outputfile}").exists()])
-        out, err = capfd.readouterr()
 
 def test_valid_input_dir():
     """
+    Test that the input directory and input files
+    exist and are valid
     """
     for num in range(3):
         outputfile = STATIC_DATA_NCFILE[num]
- 
+
         assert all([Path(f"{NCGEN_OUT}/P0Y/P0Y").is_dir(),
                     len(os.listdir(f"{NCGEN_OUT}/P0Y/P0Y")) != 0,
                     Path(f"{TEST_DIR}/ncgen_output/{COMP_NAME}/P0Y/P0Y/{outputfile}").is_file()])
 
 def test_valid_output_dir():
-#    """
-#    """
+    """
+    Test that the output directory exists and is valid
+    """
     assert all([Path(COMBINE_STATICS_OUT).exists(),
                 Path(COMBINE_STATICS_OUT).is_dir])
 
-def test_cdo_merge(monkeypatch):
-#for sucess
-#content in nc --> shows cdo merge call
+def test_combine_statics(monkeypatch):
+    """
+    Test that combine-statics script was successful
+    """
     monkeypatch.setenv("inputDir", f"{TEST_DIR}/ncgen_output")
     monkeypatch.setenv("outputDir", COMBINE_STATICS_OUT)
 
     script = f"{COMBINE_STATICS_DIR}/bin/combine-statics"
-    test = subprocess.run(script)
+    test = subprocess.run(script, check = False)
 
     assert all ([test.returncode == 0,
                  Path(f"{COMBINE_STATICS_OUT}/atmos_static_scalar/atmos_static_scalar.static.nc").is_file()])
 
 def test_cdo_merge_output_content():
     """
+    Test that netcdf output file has cdo merge call with the correct files
     """
     outfile = f"{COMBINE_STATICS_OUT}/atmos_static_scalar/atmos_static_scalar.static.nc"
 
     # read output static netcdf file
-    with netCDF4.Dataset(outfile, 'r') as sf:
-        cdo_call = "cdo -O merge atmos_static_scalar.bk1.nc atmos_static_scalar.bk2.nc atmos_static_scalar.bk3.nc /home/Dana.Singh/fre/combine-statics-test/fre-workflows/app/combine-statics/tests/combine_statics_output/atmos_static_scalar/atmos_static_scalar.static.nc"
+    with Dataset(outfile, 'r') as sf:
+        cdo_call = """cdo -O merge atmos_static_scalar.bk1.nc atmos_static_scalar.bk2.nc atmos_static_scalar.bk3.nc /home/Dana.Singh/fre/combine-statics-test/fre-workflows/app/combine-statics/tests/combine_statics_output/atmos_static_scalar/atmos_static_scalar.static.nc"""
         history_str = sf.__dict__.get("history")
 
-        assert cdo_call in history_str
+    assert all ([cdo_call in history_str,
+                 "atmos_static_scalar.bk1.nc","atmos_static_scalar.bk2.nc","atmos_static_scalar.bk3.nc"])
