54369e4d89336f81cebbd4274a635e4d5067bf22
Ian Laflotte <ian.laflotte@noaa.gov>
2025-08-12 15:59:04 -0400

    quick commit to make sure i havent done something the pipeline will resent

--- tests/test_papiex_tooler.py
+++ tests/test_papiex_tooler.py
@@ -22,8 +22,9 @@ def test_simple_failing_command():
     control_script_targ = str(Path.cwd()) + \
         '/tests/test_files_papiex_tooler/simple_failing_command.bash'
     assert Path(control_script_targ).exists() #quick check
+    print(f'control_script_targ = {control_script_targ}')
 
-    # import subprocess and run "control" process for later comparison
+    # import subprocess and run "control" process
     control_proc = None
     try:
         control_proc = Popen( args=['/bin/bash' ,control_script_targ],
@@ -35,21 +36,22 @@ def test_simple_failing_command():
         print('problem with running control script before tagging.')
         assert False
 
-    # grab control output
+    # grab captured output from the "control" process for later comparison
     control_out, control_err = None, None
     control_ret_code = None
     try:
         control_out, control_err = (
             f.decode() for f in control_proc.communicate(DEVNULL) )
         control_ret_code = control_proc.wait()
-        print(f'control_out, control_err, control_ret_code = ' + \
-              '\n {control_out}, \n {control_err}, \n {control_ret_code}')
-        assert all( [ control_out is not None,
-                      control_err is not None,
-                      control_ret_code is not None ] )        
+        print(f'control_out, control_err, control_ret_code = \n' + \
+              f'out=\n{control_out},\nret_code=\n{control_ret_code},\nerr=\n{control_err}\n')
     except OSError as exc:
         print('problem getting output from control proc')
         assert False
+    assert all( [ control_out is not None,
+                  control_err is not None,
+                  control_ret_code is not None,
+                  control_ret_code == 0 ] ) # the script returns 0 if it fails like it should
 
     # if we're testing over and over and '.notags' version exists,
     # clobber the tagged version and replace it with '.notags' version
@@ -64,7 +66,8 @@ def test_simple_failing_command():
     
     # check that output files were created as we expect
     assert Path(test_script_targ).exists()
-    
+    print(f'test_script_targ = {test_script_targ}')
+
     # now check that the script fails, as it should, after tooling
     test_proc=None
     try:
@@ -78,19 +81,19 @@ def test_simple_failing_command():
         assert False
                     
     try:
-        out, err = (
+        test_out, test_err = (
             f.decode() for f in test_proc.communicate(DEVNULL) )
-        ret_code = test_proc.wait()
-        print(f'out, err, ret_code = \n {out}, \n {err}, \n {ret_code}')        
+        test_ret_code = test_proc.wait()
     except OSError as exc:
         print('problem getting output from job-submission test_proc.')
         assert False
+    print(f'test_out, test_err, test_ret_code = \n {test_out}, \n {test_err}, \n {test_ret_code}')
 
     # check that the script output, all of it, is unchanged. 
-    assert all ( [ control_out == out,
-                   control_err == err,
-                   control_ret_code == 0, 
-                   control_ret_code == ret_code ] )
+    assert all ( [ test_out == control_out,
+                   test_err == control_err,
+                   test_ret_code == control_ret_code,
+                   test_ret_code == 0 ] )
 
 
 def test_check_simple_failing_command_for_diff():    
@@ -124,15 +127,22 @@ def test_check_simple_failing_command_for_diff():
     #      python -m pytest -rP tests/test_papiex_tooler
     # to get the same for FAILING tests,
     #      python -m pytest -rx tests/test_papiex_tooler
+    diff_lines_to_check=[]
     for line in differences:
-        if line[0]=='-' or line[0]=='+':
-            print(line)
+        if line[0]=='-':
+            diff_lines_to_check.append(line)
+        elif line[0]=='+':
+            diff_lines_to_check.append(line)
         else:
             continue
+        
+    assert diff_lines_to_check[0] == '- if mv DNE_file DNE2_file; then\n'
+    assert diff_lines_to_check[1] == '+ if { export PAPIEX_TAGS="op:mv;op_instance:1"; mv DNE_file DNE2_file; ' + \
+        'SUCCESS=$?; unset PAPIEX_TAGS; } && [ $SUCCESS -eq 0 ]; then\n'
 
-    assert is_different        
-    # better be something in here.
+    assert is_different
     assert differences is not None
+    assert diff_lines_to_check != []
     
 
 def test_rose_task_run_for_diff():    
@@ -165,14 +175,21 @@ def test_rose_task_run_for_diff():
     print(f'different? {is_different}\n\n')
 
     # now we will explicitly check for those differences
+    print(f'control_script_targ={control_script_targ}')
     the_infile = open(control_script_targ)
     infile_contents=the_infile.readlines()
     the_infile.close()
-    
+
+    print(f'control_script_targ={script_targ}')
     the_outfile = open(script_targ)
     outfile_contents=the_outfile.readlines()
     the_outfile.close()
     differences=dl.ndiff(infile_contents, outfile_contents)
+    assert differences is not None
+#    print (differences)
+#    for diff in differences:
+#        print(diff)
+    assert False
 
     # pytest suppresses print output by default.
     # to view this and other print output in pytest
@@ -231,6 +248,9 @@ def test_pp_starter_for_no_diff():
     outfile_contents=the_outfile.readlines()
     the_outfile.close()
     differences=dl.ndiff(infile_contents, outfile_contents)
+    assert differences is not None
+    print (differences)
+    assert False
 
     # pytest suppresses print output by default.
     # to view this and other print output in pytest
