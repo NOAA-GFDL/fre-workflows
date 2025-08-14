1f6c677104e62c2137dc18870e745b7e5dbcfb25
Ian Laflotte <ilaflott@gmail.com>
2025-08-14 15:20:24 -0400

    clean up first few tests and move some assert statements around

--- lib/python/tests/test_papiex_tooler.py
+++ lib/python/tests/test_papiex_tooler.py
@@ -1,9 +1,14 @@
 ''' tests for PP/AN specific ops-tooling of job scripts for PAPIEX'''
+
+import difflib as dl
+import filecmp as fc
+
 from subprocess import Popen, PIPE, DEVNULL
 from pathlib import Path
-import filecmp as fc
-import difflib as dl
+
 import pytest
+
+SIMPLE_FAILING_BASH = 'lib/python/tests/test_files_papiex_tooler/simple_failing_command.bash'
 
 def test_import():
     ''' check that tool_ops_w_papiex functions can be imported '''
@@ -19,18 +24,17 @@ def test_import_papiex_ops():
 def test_simple_failing_command():
     ''' check that setting/unsetting PAPIEX_TAGS around an op
     will not touch the exit status of that op '''
-    control_script_targ = str(Path.cwd()) + \
-        '/tests/test_files_papiex_tooler/simple_failing_command.bash'
+    control_script_targ = SIMPLE_FAILING_BASH
     assert Path(control_script_targ).exists() #quick check
 
     # import subprocess and run "control" process for later comparison
     control_proc = None
     try:
-        control_proc = Popen( args=['/bin/bash' ,control_script_targ],
-                         bufsize=0,
-                         executable=None,
-                         stdin=DEVNULL, stdout=PIPE, stderr=PIPE,
-                         preexec_fn=None, close_fds=False, shell=False)
+        control_proc = Popen( args = ['/bin/bash', control_script_targ],
+                              bufsize = 0,
+                              executable = None,
+                              stdin = DEVNULL, stdout = PIPE, stderr = PIPE,
+                              preexec_fn = None, close_fds = False, shell = False)
     except OSError as exc:
         print('problem with running control script before tagging.')
         assert False
@@ -53,7 +57,7 @@ def test_simple_failing_command():
 
     # if we're testing over and over and '.notags' version exists,
     # clobber the tagged version and replace it with '.notags' version
-    test_script_targ=control_script_targ+'.tags'
+    test_script_targ = control_script_targ+'.tags'
     if Path(test_script_targ).exists():
         Path(test_script_targ).unlink()    
 
@@ -66,13 +70,13 @@ def test_simple_failing_command():
     assert Path(test_script_targ).exists()
     
     # now check that the script fails, as it should, after tooling
-    test_proc=None
+    test_proc = None
     try:
-        test_proc=Popen( args=['/bin/bash' ,test_script_targ],
-                         bufsize=0,
-                         executable=None,
-                         stdin=DEVNULL, stdout=PIPE, stderr=PIPE,
-                         preexec_fn=None, close_fds=False, shell=False)
+        test_proc = Popen( args = ['/bin/bash', test_script_targ],
+                           bufsize = 0,
+                           executable = None,
+                           stdin = DEVNULL, stdout = PIPE, stderr = PIPE,
+                           preexec_fn = None, close_fds = False, shell = False )
     except OSError as exc:
         print('problem with sourcing test script after tagging')
         assert False
@@ -98,42 +102,45 @@ def test_check_simple_failing_command_for_diff():
     prev test, which could in theory succeed if tool_ops_w_papiex copies 
     the script but without adding tooling.'''
     
-    control_script_targ=str(Path.cwd())+'/tests/test_files_papiex_tooler/simple_failing_command.bash'
-    script_targ=control_script_targ+'.tags'
+    control_script_targ = str(Path.cwd())+SIMPLE_FAILING_BASH
     assert Path(control_script_targ).exists() #quick check
+    
+    script_targ = control_script_targ+'.tags'
     assert Path(script_targ).exists() #quick check
 
     # check quickly that they are different in some manner.
-    is_different=not fc.cmp( control_script_targ, script_targ,
-                             shallow=False)
+    is_different = not fc.cmp( control_script_targ, script_targ,
+                             shallow = False)
     print(f'different? {is_different}\n\n')
-
+    assert is_different        
+    
     # now we will explicitly check for those differencves
     the_infile = open(control_script_targ)
-    infile_contents=the_infile.readlines()
+    infile_contents = the_infile.readlines()
     the_infile.close()
+    assert infile_contents is not None
     
     the_outfile = open(script_targ)
-    outfile_contents=the_outfile.readlines()
+    outfile_contents = the_outfile.readlines()
     the_outfile.close()
-    differences=dl.ndiff(infile_contents, outfile_contents)
-
-    # pytest suppresses print output by default.
-    # to view this and other print output in pytest
-    # from PASSING tests, run something like:
-    #      python -m pytest -rP tests/test_papiex_tooler
-    # to get the same for FAILING tests,
-    #      python -m pytest -rx tests/test_papiex_tooler
-    for line in differences:
-        if line[0]=='-' or line[0]=='+':
-            print(line)
-        else:
-            continue
-
-    assert is_different        
-    # better be something in here.
-    assert differences is not None
+    assert outfile_contents is not None
     
+    # better be something in here.
+    differences = dl.ndiff(infile_contents, outfile_contents)
+    assert differences is not None
+
+    ## DEBUG
+    ## pytest suppresses print output by default.
+    ## to view this and other print output in pytest
+    ## from PASSING tests, run something like:
+    ##      python -m pytest -rP tests/test_papiex_tooler
+    ## to get the same for FAILING tests,
+    ##      python -m pytest -rx tests/test_papiex_tooler
+    #for line in differences:
+    #    if line[0] == '-' or line[0] == '+':
+    #        print(line)
+    #    else:
+    #        continue
 
 def test_rose_task_run_for_diff():    
     control_script_targ = str(Path.cwd()) + \
