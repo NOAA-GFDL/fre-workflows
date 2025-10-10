55f527da030e35e1e6286400116c02d1adec0dcb
mlee03 <Mikyung.Lee@lscamd50-d.gfdl.noaa.gov>
2025-10-10 10:37:23 -0400

    one more fix

--- for_gh_runner/runscript.sh
+++ for_gh_runner/runscript.sh
@@ -22,6 +22,7 @@ source /opt/conda/etc/profile.d/conda.sh
 conda deactivate
 conda activate /app/cylc-flow-tools
 
+# update fre-cli env with specific branch development
 git clone --recursive https://github.com/NOAA-GFDL/fre-cli
 cd fre-cli
 pip install .
