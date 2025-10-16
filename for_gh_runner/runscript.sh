3e3e5ffb272439b15d12c663c5a6de94d440b93b
Ian Laflotte <ilaflott@gmail.com>
2025-10-16 10:59:25 -0400

    more output for gh_runner runscript.sh

--- for_gh_runner/runscript.sh
+++ for_gh_runner/runscript.sh
@@ -113,6 +113,10 @@ fre_pp_steps () {
 
     # Put log in output file
     cylc cat-log ${name} > "/mnt/log.out"
+    echo "" >> "mnt/log.out"
+    echo "" >> "mnt/log.out"
+    echo "" >> "mnt/log.out"
+    cylc workflow-state ${name} >> "/mnt/log.out"
     check_exit_status "Writing to log.out"
 }
 
