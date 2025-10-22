29ae76ece5afd115deed161cf5a314419b33490a
Ian Laflotte <ilaflott@gmail.com>
2025-10-22 15:59:46 -0400

    update testing condition for analysis_validator and fend with some of pylints easier complaints

--- tests/test_analysis_validator.py
+++ tests/test_analysis_validator.py
@@ -13,4 +13,9 @@ def test_skip_analysis_validator():
    config=config_node.set(keys=['template variables','DO_ANALYSIS_ONLY'], value='False')
    output = validator.validate(config=config,
                                meta_config=None)
-   assert ("Not doing analysis. Moving on." in str(output) )
+   ## old condition, well-intentioned, but any report == an error. this is just info 
+   #   assert ("Not doing analysis. Moving on." in str(output) )
+
+   # new condition, opaque, but doesn't throw an error and represents an absence of action taken
+   # which is the more important desired effect with both keys False
+   assert [] == output
