41693aaf6facb16739416cef0652553522dce2b5
Ian Laflotte <ilaflott@gmail.com>
2025-11-07 10:45:41 -0500

    revamp lone test to pylint standards

--- tests/test_analysis_validator.py
+++ tests/test_analysis_validator.py
@@ -1,16 +1,21 @@
-#test analysis_validator macro
+'''
+tests for analysis_validator
+'''
 
-import pytest
-from meta.lib.python.macros.analysis_validator import Analysis_Validator
-from metomi.rose.config import load, ConfigNode 
-import configparser
+from metomi.rose.config import ConfigNode
+
+from meta.lib.python.macros.analysis_validator import Analysis_Validator # pylint: disable=import-error
 
 def test_skip_analysis_validator():
-   '''checks that analysis validation is skipped if no analysis is requested.'''
-   validator = Analysis_Validator()
-   config_node = ConfigNode()
-   config=config_node.set(keys=['template variables','DO_ANALYSIS'], value='False')
-   config=config_node.set(keys=['template variables','DO_ANALYSIS_ONLY'], value='False')
-   output = validator.validate(config=config,
-                               meta_config=None)
-   assert [] == output
+    '''
+    checks that analysis validation passes no analysis is requested. 
+    
+    passing here means an empty list, i.e. no "reports"
+    '''
+    validator = Analysis_Validator()
+    config_node = ConfigNode()
+    config = config_node.set( keys = ['template variables', 'DO_ANALYSIS'], value = 'False' )
+    config = config_node.set( keys = ['template variables', 'DO_ANALYSIS_ONLY'], value = 'False' )
+    output = validator.validate( config = config,
+                                 meta_config = None )
+    assert [] == output
