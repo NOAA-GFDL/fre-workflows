656515e31d656b3875212d34f727e2e57716ae1d
Dana Singh <Dana.Singh@ldt-4742397.gfdl.noaa.gov>
2026-02-18 16:13:31 -0500

    #203 Add cylc pre-configure plugin

---
+++ install-plugin/hello_world.py
@@ -0,0 +1,27 @@
+from cylc.flow import LOG
+from pathlib import Path
+import yaml
+
+def pre_configure(srcdir=None, opts=None, rundir="/home/Dana.Singh/cylc-run"):
+    LOG.info("PLUGIN OUTPUT")
+    LOG.info("Hello world. How the heck are you?")
+
+    yml_info = {}
+    yaml_file = Path(f"{srcdir}/resolved.yaml")
+    if yaml_file:
+        yf = yaml_file #[0]
+        with open(yf, 'r') as f:
+            yml_info = yaml.safe_load(f)
+
+    LOG.info(yml_info)
+    return {'template_variables': yml_info,
+            'templating_detected': 'jinja2'}
+
+## want this script to accept resolved yaml and create everything needed to run workflow
+# - in dictionary format
+# - gets rid of rose-suite.conf
+# - removes dependency on cylc-rose...I think
+
+## questions/notes:
+# - can function in this script be anything? or does it need to be post_install to be recognized? (as a plugin type)
+# - I think it can be anything as long as its registered under one of the plugin types in setup.py 
---
+++ setup.py
@@ -0,0 +1,17 @@
+# plugins must be properly installed, in-place PYTHONPATH meddling will not work.
+
+from setuptools import setup
+
+## Register plugin
+setup(
+    name='hello-world',
+    version='1.0',
+    py_modules=['hello_world'], #name of file
+    entry_points={
+        # group name: ["plugin_name = module:function"]
+        'cylc.pre_configure': [
+            'hw = hello_world:pre_configure'
+        ],
+    }
+)
+
